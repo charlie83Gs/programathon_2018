cf063af1c035cb2a12e8be97a1b58f7fff531b63
charlie <carlosgomezsoza@gmail.com>
2018-09-22 15:21:15 -0600

    create procedures script and install of mysql driver

--- SqlScripts/createDatabasse.sql
+++ SqlScripts/createDatabasse.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE programathon;
+CREATE DATABASE IF NOT EXISTS programathon;
 USE programathon;
 
-CREATE TABLE usuario(
-	id_usuario INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS usuario(
+	id_usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	password_diggest VARCHAR(256),
 	telefono VARCHAR(40),
 	nombre VARCHAR(40),
@@ -12,31 +12,22 @@ CREATE TABLE usuario(
 	activo BOOL
 );
 
-ALTER TABLE usuario
-ADD PRIMARY KEY (id_usuario);
 
-
-CREATE TABLE etnia(
-	id_etnia INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS etnia(
+	id_etnia INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	nombre varchar(40)
 );
 
-ALTER TABLE etnia
-ADD PRIMARY KEY (id_etnia);
 
-
-CREATE TABLE paciente(
-	id_paciente INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS paciente(
+	id_paciente INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	id_usuario INT NOT NULL,
 	cedula VARCHAR(20),
 	edad SMALLINT,
-	genero varchar,
+	genero VARCHAR(20),
 	id_etnia INT NOT NULL,
 	activo BOOL
 );
-
-ALTER TABLE paciente
-ADD PRIMARY KEY (id_paciente);
 
 ALTER TABLE paciente
 ADD FOREIGN KEY (id_usuario) 
@@ -47,36 +38,31 @@ ADD FOREIGN KEY (id_etnia)
 REFERENCES etnia(id_etnia);
 
 
-CREATE TABLE enfermedad(
-	id_enfermedad INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS enfermedad(
+	id_enfermedad INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	id_paciente INT NOT NULL,
 	detalle VARCHAR(4096),
 	activo BOOL
 );
 
-ALTER TABLE enfermedad
-ADD PRIMARY KEY (id_enfermedad);
  
  ALTER TABLE enfermedad
 ADD FOREIGN KEY (id_paciente) 
-REFERENCES id_paciente(id_paciente);
+REFERENCES paciente(id_paciente);
 
-CREATE TABLE tratamiento(
-	id_tratamiento INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS tratamiento(
+	id_tratamiento INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	id_paciente INT NOT NULL,
 	detalle VARCHAR(4096),
 	activo BOOL
 );
-
-ALTER TABLE tratamiento
-ADD PRIMARY KEY (id_tratamiento);
 
 ALTER TABLE tratamiento
 ADD FOREIGN KEY (id_paciente) 
 REFERENCES paciente(id_paciente);
 
-CREATE TABLE medicina(
-	id_medicina INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS medicina(
+	id_medicina INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	id_tratamiento INT NOT NULL,
 	nombre VARCHAR(512),
 	dosis  VARCHAR(512),
@@ -86,22 +72,18 @@ CREATE TABLE medicina(
 	activo BOOL
 );
 
-ALTER TABLE medicina
-ADD PRIMARY KEY (id_medicina);
 
 ALTER TABLE medicina
 ADD FOREIGN KEY (id_tratamiento) 
 REFERENCES tratamiento(id_tratamiento);
 
-CREATE TABLE tratamientoFisico(
-	id_tratamiento_fisico INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS tratamientoFisico(
+	id_tratamiento_fisico INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	id_tratamiento INT NOT NULL,
 	detalle VARCHAR(4096),
 	activo BOOL
 );
 
-ALTER TABLE tratamientoFisico
-ADD PRIMARY KEY (id_tratamiento_fisico);
 
 ALTER TABLE tratamientoFisico
 ADD FOREIGN KEY (id_tratamiento) 
