0d58a643cc913570c5c2446e5028faa68ee660ce
Bryan Hernández Sibaja <bryan_2504@hotmail.com>
2018-09-23 04:29:06 -0600

    arreglo de interfaz

--- ServidorWeb/src/view/registro_antecedentes/registro_antecedentes.js
+++ ServidorWeb/src/view/registro_antecedentes/registro_antecedentes.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
-import { Button, FormGroup, FormControl, ControlLabel,Radio,Col, InputGroup,Modal } from "react-bootstrap";
+import { Button, FormGroup, FormControl, ControlLabel,Radio,Col, InputGroup,Modal, Table } from "react-bootstrap";
 import './registro_antecedentes.css';
+
+export const caract = {
+  background: "#128056",
+  color: "#FFFFFF"
+}
 
 class registro_antecedentes extends Component {
   constructor(props, context) {
@@ -20,33 +25,78 @@ class registro_antecedentes extends Component {
   render() {
     return (
       <div className="modal-container" style={{ height: 200 }}>
-        <Button
-          bsStyle="primary"
-          bsSize="large"
-          onClick={() => this.setState({ show: true })}
-        >
-          Selecciona el tipo de antecedente
-        </Button>
+        <div className={"center-div-antecedentes"}><br/>
+          <h1 className={"centTitleSign"}>Antecedentes de un Paciente</h1>
 
-        <Modal
-          show={this.state.show}
-          onHide={this.handleHide}
-          container={this}
-          aria-labelledby="contained-modal-title"
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title">
-              Contained Modal
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-Seleccione el tipo de archivo en el que quiere importar el antecedente. El archivo soporta los siguientes formatos: pdf, txt, docx, jpg, png y tiff.
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.handleHide}>Archivo</Button>
-            <Button onClick={this.handleHide}>Enlace</Button>
-          </Modal.Footer>
-        </Modal>
+          <div>
+
+            <FormGroup align="center" controlId="formControlsSelect">
+              <ControlLabel className={"tablaDim"}>Seleccione a su Paciente</ControlLabel>
+              <FormControl className={"tablaDim"} componentClass="select" placeholder="select">
+                <option value="other">aca se cargan los niños</option>
+              </FormControl><br/>
+            </FormGroup>
+
+
+            <Table align="center" className={"tablaDim"} striped bordered condensed hover>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Nombre del Archivo</th>
+                  <th>Descripción</th>
+                  <th> </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>
+                    <Button className={"tamButtom"}  block bsSize="small" type="submit" onClick={this.validateSession}> Borrar</Button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td colSpan="2">Larry the Bird</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        
+
+          <div align="center">
+            <Button style={caract}  bsSize="large" onClick={() => this.setState({ show: true })}>
+              Agregar un tipo de antecedente
+            </Button>
+          </div>
+
+
+          <Modal
+            show={this.state.show}
+            onHide={this.handleHide}
+            container={this}
+            aria-labelledby="contained-modal-title"
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title">
+                Formato de Antecedente
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+  Seleccione el tipo de archivo en el que quiere importar el antecedente. El archivo soporta los siguientes formatos: pdf, txt, docx, jpg, png y tiff.
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={this.handleHide}>Archivo</Button>
+              <Button onClick={this.handleHide}>Enlace</Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
       </div>
     );
   }
