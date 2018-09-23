3aeeec37274197915c90d99340ae6f9bc4729073
reyesm2 <reyes.marlon57@gmail.com>
2018-09-23 05:55:58 -0600

    procedimientosTrataMedic

---
+++ ServidorApi/programathon_api/routes/getEtnias.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../src/controller/dbController.js');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	db.getEtnias(function(result){
+		console.log(result);
+	
+	});
+
+});
+
+module.exports = router;
